e3d2ae5e02e3d6b95c0635b18d177ad274cbff9f
Simo Elalj <simo@elalj.com>
2021-11-18 12:00:46 +0100

    using /lastDate endpoint

--- api/index.js
+++ api/index.js
@@ -3,7 +3,11 @@ import { context as ctx } from '@tonoid/helpers';
 export default ({ getRouter, asyncHandler }) => {
   const router = getRouter();
 
-  router.get('/', asyncHandler(async (req, res) => {
+  router.get('/', asyncHandler((req, res) => {
+    res.send({ index: true });
+  }));
+
+  router.get('/lastDate', asyncHandler(async (req, res) => {
     const lastDate = await ctx.redis.getValue('lastDate');
     await ctx.redis.setValue('lastDate', new Date().toISOString());
     res.send({ lastDate });
